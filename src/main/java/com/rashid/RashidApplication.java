40a331952de39618a15d1f6f4e7b0a8e9f9085fb
Rashid Aidun <raidun@yahoo.com>
2018-01-26 17:47:52 -0800

    First

---
+++ src/main/java/com/rashid/RashidApplication.java
@@ -0,0 +1,12 @@
+package com.rashid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RashidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RashidApplication.class, args);
+	}
+}
